2672a6078fcc3a3adeaa12ef10a4725bab013944
Your Name <haciyevqurban97@gmail.com>
2023-12-24 03:20:17 +0400

    zod library config fixed

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,7 +1,56 @@
+"use client"
+ import { Button } from "@/components/ui/button"
+import { SignupValidation } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {useForm} from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import {z} from "zod";
+const SignupForm = () => {  
 
-const SignupForm = () => {
+    const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  function onSubmit(values: z.infer<typeof SignupValidation>) {
+  
+    console.log(values)
+  }
   return (
-    <div>SignupForm</div>
+    <>
+   <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+        </>
   )
 }
 
