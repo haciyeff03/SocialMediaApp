68af591c963072764b9baf4fd51412dcc408e3fc
Your Name <haciyevqurban97@gmail.com>
2023-12-24 01:52:34 +0400

    auth and root folder complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import { Routes,Route } from "react-router-dom";
 import "./globals.css";
-
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import {Home} from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 const App = () => {
   return (
     <main className="flex h-screen">
+        <Routes>    
+            { /* public routes */}
+            <Route element={<AuthLayout/>}/>
+            <Route path="/sign-in" element={<SigninForm/>}/>
+            <Route path="/sign-up" element={<SignupForm/>}/>
 
+            { /* private routes */}
+            <Route  index element={<RootLayout/>}/>
+            <Route  element={<Home/>}/>
+        </Routes>
     </main>
   )
 }
