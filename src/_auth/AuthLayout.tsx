200adf3ea8fdbe868227d681727ffdccef8f950a
Your Name <haciyevqurban97@gmail.com>
2024-01-21 05:06:47 +0400

    home page important functions

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,8 +1,9 @@
 import { Outlet, Navigate } from "react-router-dom";
+
 import { useUserContext } from "@/context/AuthContext";
 
-const AuthLayout = () => {
-  const isAuthenticated = false;
+export default function AuthLayout() {
+  const { isAuthenticated } = useUserContext();
 
   return (
     <>
@@ -13,6 +14,7 @@ const AuthLayout = () => {
           <section className="flex flex-1 justify-center items-center flex-col py-10">
             <Outlet />
           </section>
+
           <img
             src="/assets/images/side-img.svg"
             alt="logo"
@@ -22,6 +24,4 @@ const AuthLayout = () => {
       )}
     </>
   );
-};
-
-export default AuthLayout;
+}
