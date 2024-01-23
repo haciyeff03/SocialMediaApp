321a1d14088e9efe4f946c9b5e7614a3973e8a33
Your Name <haciyevqurban97@gmail.com>
2024-01-23 17:21:09 +0400

    bottombar section #1

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -80,17 +80,17 @@ const SignupForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col ">
-        <img src="/assets/images/logo.svg" alt="logo" />
-        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+        <img src="/assets/images/logo.svg" alt="logo" className="mr-4 mt-4"/>
+        <h2 className="h3-bold md:h2-bold pt-2 sm:pt-12">
           Create a new account
         </h2>
-        <p className="text-light-3 small-medium md:base-regular mt-2">
+        <p className="text-light-3 small-medium md:base-regular mt-1">
           To use Snapgram, please enter your details
         </p>
 
         <form
           onSubmit={form.handleSubmit(handleSignup)}
-          className="flex flex-col gap-5 w-full mt-4"
+          className="flex flex-col gap-5 w-full mt-2"
         >
           <FormField
             control={form.control}
--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,7 +1,38 @@
-
+import { Link,useLocation } from "react-router-dom"
+import { bottombarLinks } from "@/constants"
 const Bottombar = () => {
+  const {pathname}=useLocation();
   return (
-    <div>Bottombar</div>
+  <section className="bottom-bar">
+ {bottombarLinks.map((link) => {
+            const isActive = pathname === link.route;
+
+            return (
+             
+                <Link
+                  to={link.route}
+                  key={link.label}
+                className={` ${
+                  isActive && "bg-primary-500 rounded-[10px]"
+                } flex-center flex-col p-2 gap-1 transition`}
+                >
+                  <img
+                    src={link.imgURL}
+                    alt={link.label}
+                    width={16}
+                    height={16}
+                    className={`${
+                      isActive && "invert-white"
+                    }`}
+                  />
+                 <p className="text-light-2 tiny-big ">
+                {link.label}
+                </p> 
+                </Link>
+              
+            );
+          })}
+  </section>
   )
 }
 
