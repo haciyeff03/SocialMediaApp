3207599ea0bf2b2bbd7a74dc6f463c82bd04ada1
Qurban Haciyev <haciyevqurban97@gmail.com>
2024-02-08 19:56:33 +0400

    react-dropzone fixed

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,8 +1,22 @@
-
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
 const FileUploader = () => {
+
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+
   return (
-    <div>FileUploader</div>
-  )
+ <div {...getRootProps()} className='flex flex-center flex-col bg-dark-3 rounded-1x  cursor-pointer'>
+      <input {...getInputProps()} className='cursor-pointer'/>
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>  )
 }
 
 export default FileUploader
