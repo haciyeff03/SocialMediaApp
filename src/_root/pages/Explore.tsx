1cb317411482790026a00c7f6e235227446edc90
Qurban Haciyev <haciyevqurban97@gmail.com>
2024-02-12 23:01:14 +0400

    explore page update

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -62,10 +62,8 @@ const Explore = () => {
           />
         </div>
       </div>
-
       <div className="flex-between w-full max-w-5xl mt-16 mb-7">
         <h3 className="body-bold md:h3-bold">Popular Today</h3>
-
         <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
           <p className="small-medium md:base-medium text-light-2">All</p>
           <img
@@ -76,7 +74,6 @@ const Explore = () => {
           />
         </div>
       </div>
-
       <div className="flex flex-wrap gap-9 w-full max-w-5xl">
         {shouldShowSearchResults ? (
           <SearchResults
@@ -91,7 +88,6 @@ const Explore = () => {
           ))
         )}
       </div>
-
       {hasNextPage && !searchValue && (
         <div ref={ref} className="mt-10">
           <Loader />
@@ -100,5 +96,4 @@ const Explore = () => {
     </div>
   );
 };
-
 export default Explore;
