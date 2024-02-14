53cb4b43247e1090e18c19cb7e7093e844a9a238
Qurban Haciyev <haciyevqurban97@gmail.com>
2024-02-15 01:54:00 +0400

    constant update

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,11 +1,11 @@
 import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
 
 import { INavLink } from "@/types";
-import { sidebarLinks } from "@/constants";
 import Loader from "./Loader";
 import { Button } from "@/components/ui/button";
 import { useSignOutAccount } from "@/lib/react-query/queries";
 import { useUserContext, INITIAL_USER } from "@/context/AuthContext";
+import { sidebarLinks } from "../constants";
 
 const LeftSidebar = () => {
   const navigate = useNavigate();
